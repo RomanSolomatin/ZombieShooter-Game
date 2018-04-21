75d6b9c26d2744aacb85e2ac6a3e3c746bcd4eb8
Roman Solomatin <roman.solomatin@gmail.com>
2018-04-21 23:06:29 +0300

    Removed useless code.

--- Source/ZombieShooter.Target.cs
+++ Source/ZombieShooter.Target.cs
@@ -9,17 +9,4 @@ public class ZombieShooterTarget : TargetRules
 	{
 		Type = TargetType.Game;
 	}
-
-	//
-	// TargetRules interface.
-	//
-
-	public override void SetupBinaries(
-		TargetInfo Target,
-		ref List<UEBuildBinaryConfiguration> OutBuildBinaryConfigurations,
-		ref List<string> OutExtraModuleNames
-		)
-	{
-		OutExtraModuleNames.AddRange( new string[] { "ZombieShooter" } );
-	}
 }
